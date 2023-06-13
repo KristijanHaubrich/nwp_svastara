17195f05b54ded94b6cd27de49c170983da22fe0
KristijanHaubrich <kiki.hauba@gmail.com>
2023-06-13 10:23:38 +0200

    fixed cors error

--- nwp_frontend_svastara/src/Login.jsx
+++ nwp_frontend_svastara/src/Login.jsx
@@ -27,24 +27,12 @@ const LoginPage = () => {
   const handleSubmit =  async (e) => {
     e.preventDefault()
 
-    // const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b21vLm1hZGphckBnbWFpbC5jb20iLCJpYXQiOjE2ODYyNTI1NDEsImV4cCI6MTY4NzExNjU0MX0.Dvps1W7uuNI3AFdEieNJzQvEmTC_HQaGhT9_Yu2jj44"
-    // const isTokenExpired = checkTokenExpiration(token)
-    
-    // if(isTokenExpired){
-    //   navigation("/login")
-    // }else{
-    //   const response= await apiRequest(token).get(`/clients`)
-    //   console.log(response.data)
-    // }
-
     const body = {password:password,email:email}
     const response = await apiRequest("").post("/public/authenticate",body)
    
     if(response?.data){
       if(response.data.isClientFound && response.data.isPasswordCorrect){
-        //ulogiraj korisnika
         dispatch(login())
-        console.log(response.data.client)
         dispatch(setClientData({data:response.data.client}))
         navigation("/clientPage")
       }else if(!response.data.isClientFound){
