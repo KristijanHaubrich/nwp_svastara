f450ec082ae3414cd37369560392cd62fe057b06
KristijanHaubrich <kiki.hauba@gmail.com>
2023-06-18 20:43:55 +0200

    delete product

--- nwp_frontend_svastara/src/Products.jsx
+++ nwp_frontend_svastara/src/Products.jsx
@@ -6,27 +6,45 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { setClientData } from "./redux/clientReducer";
 import { useDispatch } from "react-redux";
+import checkTokenExpiration from "./utils/checkTokenExpiration";
+import { useNavigate } from "react-router-dom";
+import { logout } from "./redux/loginReducer";
+import { clearClientData } from "./redux/clientReducer";
 
 const Products = ({ product }) => {
   const client = useSelector((state) => state.client.data);
   const token = client.accessToken;
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const logoff = () => {
+    dispatch(logout())
+    dispatch(clearClientData())
+    navigate("/login")
+  }
+
   const deleteProduct = async () => {
+
     try {
-      const response = await apiRequest(token).delete(`products/${product.name}`);
-      if(response){
-        if(response.data.validate){
-          //dohvacanje podataka o klijentu s novim podacima
-          const response = await apiRequest(token).get(`/clients/${client.email}`)
-          if(response?.data){
-            //updatanje podataka o klijentu na lokalnoj bazi
-            if(response.data.isClientFound){
-              dispatch(setClientData({data:response.data.client}))
-              window.location.reload(true)
+      if(checkTokenExpiration(token)){
+        logoff()
+      }else{
+        const response = await apiRequest(token).delete(`products/${product.name}`);
+        if(response){
+          if(response.data.validate){
+            //dohvacanje podataka o klijentu s novim podacima
+            const response = await apiRequest(token).get(`/clients/${client.email}`)
+            if(response?.data){
+              //updatanje podataka o klijentu na lokalnoj bazi
+              if(response.data.isClientFound){
+                dispatch(setClientData({data:response.data.client}))
+                window.location.reload(true)
+              }
             }
           }
         }
       }
+    
     } catch (error) {
 
       toast.error("Error happened!");
